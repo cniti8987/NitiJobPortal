c717a1d635e76d9014c2649cd55e9d966dacd7e3
niti <cniti8990@gmail.com>
2020-07-02 18:56:51 -0400

    Original pro

---
+++ CareerCloud.ADODataAccessLayer/BaseAdo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public class BaseAdo
+    {
+        public const string connectionString = @"Data Source=LAPTOP-PQDRBPJ0\HUMBERBRIDGING;Initial Catalog=JOB_PORTAL_DB;Integrated Security=True;Connect Timeout=30;";
+    }
+}
